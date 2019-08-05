15566887682ba1329f969b3e8fd8bb2e34e8e27f
Florian Ressel <florian@genxtreme.de>
2019-08-05 11:18:46 +0200

    stop the multiple processing

--- backlog.sh
+++ backlog.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # bash script for backlog processing
 # cd /your/Shopware-Verzeichnis/
-amount=$(php bin/console plentyconnector:backlog:info | cat | head -4l | tail -1 | cut -d' ' -f7)
-#echo $amount;
-while [ $amount -gt 0 ] ; do
-	echo "amount queued: $amount"
-	php bin/console plentyconnector:backlog:process
-	amount=$(php -d memoryLimit=8192M bin/console plentyconnector:backlog:info | cat | head -4l | tail -1 | cut -d' ' -f7)
-done
-echo "processing finished"
+PIDFILE=/tmp/plentyconnector_backlog.pid
+
+if [ -f $PIDFILE ]
+then
+	echo "The process is already running."
+	exit 1
+else
+	echo $$ > $PIDFILE
+
+	amount=$(php bin/console plentyconnector:backlog:info | cat | head -4l | tail -1 | cut -d' ' -f7)
+	#echo $amount;
+	while [ $amount -gt 0 ] ; do
+		echo "amount queued: $amount"
+		php bin/console plentyconnector:backlog:process
+		amount=$(php -d memoryLimit=8192M bin/console plentyconnector:backlog:info | cat | head -4l | tail -1 | cut -d' ' -f7)
+	done
+	echo "processing finished"
+
+	rm $PIDFILE
+fi
